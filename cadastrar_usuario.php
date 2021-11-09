326ff2ea1ec35bc6e48460b58b5ac99f4534d354
NeiVcs <62248889+NeiVcs@users.noreply.github.com>
2021-11-09 11:37:34 -0300

    teste

--- cadastrar_usuario.php
+++ cadastrar_usuario.php
@@ -10,7 +10,6 @@ include("conexao.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Só Games</title>
 
-    
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
